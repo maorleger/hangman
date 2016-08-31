861117ae6a6881723e1371750cb753031c5b8fda
Maor Leger <maor.leger@gmail.com>
2016-08-31 12:35:25 -0600

    added puzzle code

--- src/Main.hs
+++ src/Main.hs
@@ -34,7 +34,36 @@ randomWord wl = do
   return $ wl !! randomIndex
 
 randomWord' :: IO String
-randomWord' = gameWords <<= randomWord
+randomWord' = gameWords >>= randomWord
+
+data Puzzle = Puzzle String [Maybe Char] [Char]
+instance Show Puzzle where
+  show (Puzzle _ discovered guessed) =
+    (intersperse ' ' $ fmap renderPuzzleChar discovered)
+    ++ " Guessed so far: " ++ guessed
+
+renderPuzzleChar :: Maybe Char -> Char
+renderPuzzleChar (Just c) = c
+renderPuzzleChar Nothing = '_'
+
+freshPuzzle :: String -> Puzzle
+freshPuzzle word = Puzzle word getMaybeChars []
+    where getMaybeChars = map (\x -> Nothing) word
+
+charInWord :: Puzzle -> Char -> Bool
+charInWord (Puzzle str _ _) = flip elem str
+
+alreadyGuessed :: Puzzle -> Char -> Bool
+alreadyGuessed (Puzzle _ _ guesses) = flip elem guesses
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word filledInSoFar s) c = 
+  Puzzle word newFilledInSoFar (c : s) 
+  where zipper guessed wordChar guessChar = 
+          if wordChar == guessed
+          then Just wordChar
+          else guessChar
+        newFilledInSoFar = zipWith (zipper c) word filledInSoFar
 
 main :: IO ()
 main = do
