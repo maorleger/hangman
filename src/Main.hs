700ef98cae1d291c76abeb7800556a2290fb3284
Maor Leger <maor.leger@gmail.com>
2016-08-31 18:24:58 -0600

    using newType WordList

--- src/Main.hs
+++ src/Main.hs
@@ -13,24 +13,24 @@ minWordLength = 5
 maxWordLength :: Int
 maxWordLength = 9
 
-type WordList = [String]
+newtype WordList = WordList [String] deriving (Eq, Show)
+
 allWords :: IO WordList
 allWords = do
   dict <- readFile "data/dict.txt"
-  return (lines dict)
+  return $ WordList (lines dict)
 
 gameWords :: IO WordList
 gameWords = do
-  aw <- allWords
-  return (filter gameLength aw)
+  (WordList aw) <- allWords
+  return $ WordList (filter gameLength aw)
   where gameLength w = 
           let l = length (w :: String)
           in l > minWordLength && l < maxWordLength
 
 randomWord :: WordList -> IO String
-randomWord wl = do
-  gw <- gameWords
-  randomIndex <- randomRIO (0, (length gw))
+randomWord (WordList wl) = do
+  randomIndex <- randomRIO (0, (length wl) - 1)
   return $ wl !! randomIndex
 
 randomWord' :: IO String
